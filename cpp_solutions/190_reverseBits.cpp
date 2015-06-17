9b0dc25d9cba193e725efc463ea07bb2ad135f17
freesouls <declanxu@126.com>
2015-06-17 19:27:04 +0800

    6.16

--- cpp_solutions/190_reverseBits.cpp
+++ cpp_solutions/190_reverseBits.cpp
@@ -3,7 +3,7 @@ public:
     uint32_t reverseBits(uint32_t n) {
         int res = 0;
         for (int i = 0; i < 32; i++){
-            res = (res << 1) + n%2;
+            res = (res << 1) + n%2; // n%2 is slower use something like n&1 will faster
             n = n >> 1;
         }
         return res;
