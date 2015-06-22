b3506476012fb737e3dc83ce31f50f94311826f0
freesouls <declanxu@gmail.com>
2015-06-22 11:23:09 +0800

    6.22

---
+++ cpp_solutions/72_editDistance.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int m = word1.length();
+        int n = word2.length();
+        // if (m > n){
+        //     return minDistance(word2, word1);
+        // }
+        int d[n+1];
+        for(int i = 0; i < n+1; i++){
+            d[i] = i;
+        }
+        int upper_left = 0;
+        for (int i = 1; i <= m; i++){
+            upper_left = d[0];
+            d[0] = i;
+            for (int j = 1; j <= n; j++){
+                int tmp = d[j];
+                if (word1[i-1] == word2[j-1]){
+                    d[j] = upper_left;
+                }
+                else{
+                    d[j] = 1 + min(min(d[j-1], d[j]), upper_left);
+                }
+                upper_left = tmp;
+            }
+        }
+        return d[n];
+    }
+};
