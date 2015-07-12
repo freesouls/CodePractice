1ed741a5129f7c35535bb32c29fd723ea9b42771
freesouls <declanxu@gmail.com>
2015-07-12 13:02:23 +0800

    7.12

---
+++ cpp_solutions/220_containsDuplicateIII.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyAlmostDuplicate(vector<int>& nums, int k, int t) {
+        set<long> window; // set is ordered automatically
+        for (int i = 0; i < nums.size(); i++) {
+            if (i > k) window.erase(nums[i-k-1]); // keep the set contains nums i j at most k
+            // -t <= x - nums[i] <= t;
+            set<long>::iterator pos = window.lower_bound((long)nums[i] - (long)t); // x >= nums[i] - t
+            if (pos != window.end() && *pos <= (long)nums[i] + (long)t) // x <= nums[i] + t
+                return true;
+            window.insert(nums[i]);
+        }
+        return false;
+    }
+};
