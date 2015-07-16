fe3b19e5dba3e7a9822b16a754cc26393903f365
freesouls <declanxu@126.com>
2015-07-16 15:00:10 +0800

    7.16

---
+++ cpp_solutions/238_productOfArrayExceptSelf.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<int> res(n, 1);
+
+        int pro = 1;
+        for(int i = 0; i < n; i++){
+            res[i] *= pro;
+            pro *= nums[i];
+        }
+
+        pro = 1;
+        for(int i = n-1; i >= 0; i--){
+            res[i] *= pro;
+            pro *= nums[i];
+        }
+
+        return res;
+    }
+};
