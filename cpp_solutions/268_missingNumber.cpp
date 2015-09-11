8b742467dd15c97fde8ed3e202eca99d91c7e71a
Binbin Xu <declanxu@126.com>
2015-09-11 15:45:30 +0800

    9.11

--- cpp_solutions/268_missingNumber.cpp
+++ cpp_solutions/268_missingNumber.cpp
@@ -2,6 +2,20 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         int n = nums.size();
+        int res = n;
+        for (int i = 0; i < n; i++){
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    //may be overflow
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
         int sum = n*(n+1)/2;
         for (int i = 0; i < n; i++){
             sum -= nums[i];
