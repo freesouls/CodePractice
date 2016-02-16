e83e7a888dc6ff28b1ad691cb7f7333732a93a14
freesouls <declanxu@126.com>
2016-02-16 20:40:44 +0800

    leetcode 334

--- leetcode/cpp_solutions/334_increasingTripletSubsequence.cpp
+++ leetcode/cpp_solutions/334_increasingTripletSubsequence.cpp
@@ -1,23 +1,13 @@
 class Solution {
 public:
     bool increasingTriplet(vector<int>& nums) {
-        // if (nums.size() < 3) {
-        //     return false;
-        // }
-
-        // int a = std::min(nums[0], nums[1]);
-        // // int a = nums[0];
-        // int b = nums[1];
-        // bool flag = a < b;
         int a = INT32_MAX, b = INT32_MAX;
         for (int i = 0; i < nums.size(); i++) {
             if (nums[i] <= a) {
                 a = nums[i];
-                // continue;
             }
             else if (nums[i] <= b) {
                 b = nums[i];
-                // continue;
             }
             else {
                 return true;
