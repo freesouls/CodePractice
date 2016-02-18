99e97d0692cf9c0e5727e7fdc0046b22d8bfac23
freesouls <declanxu@126.com>
2016-02-18 21:14:56 +0800

    update leetcode 145

--- leetcode/cpp_solutions/145_binaryTreePostorderTraversal.cpp
+++ leetcode/cpp_solutions/145_binaryTreePostorderTraversal.cpp
@@ -7,6 +7,29 @@
  *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
  * };
  */
+
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> res;
+        TreeNode* cur = root;
+        stack<TreeNode*> st;
+        while(cur != NULL || !st.empty()) {
+            while (cur) {
+                res.push_back(cur->val);
+                st.push(cur);
+                cur = cur->right;
+            }
+            TreeNode* node = st.top();
+            st.pop();
+            cur = node->left;
+        }
+
+        std::reverse(res.begin(), res.end());
+        return res;
+    }
+};
+
 class Solution {
 public:
     // change left and right in the preorder version, and finally reverse the vector
@@ -38,7 +61,7 @@ public:
         std::reverse(res.begin(), res.end()); // it cost time, so use next version
         return res;
     }
-    
+
     // vector<int> postorderTraversal(TreeNode* root) {
     //     vector<int> res;
     //     if (root == NULL) return res;
@@ -63,14 +86,14 @@ public:
     //     }
     //     return res;
     // }
-    
+
     // vector<int> postorderTraversal(TreeNode* root) {
     //     vector<int> res;
     //     if (root == NULL) return res;
     //     postorder(root, res);
     //     return res;
     // }
-    
+
     // void postorder(TreeNode* node, vector<int>& res){
     //     if (node->left) postorder(node->left, res);
     //     if (node->right) postorder(node->right, res);
