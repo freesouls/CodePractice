1aa5608b0d5a38f27877f1e428bb9241b40ba22d
freesouls <declanxu@126.com>
2016-02-24 22:26:25 +0800

    update backpack

--- lintcode/DynamicProgramming/BackPack.cpp
+++ lintcode/DynamicProgramming/BackPack.cpp
@@ -19,3 +19,26 @@ public:
         return dp[m];
     }
 };
+
+class Solution {
+public:
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A & V: Given n items with size A[i] and value V[i]
+     * @return: The maximum value
+     */
+    int backPack(int m, vector<int> A) {
+        // write your code here
+        vector<vector<int> > dp(A.size()+1, vector<int>(m+1, 0));
+
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < std::min(A[i], m+1); j++) {
+                dp[i+1][j] = dp[i][j];
+            }
+            for (int j = A[i]; j <= m; j++) {
+                dp[i+1][j] = std::max(dp[i][j], dp[i][j-A[i]] + A[i]);
+            }
+        }
+        return dp[A.size()][m];
+    }
+};
--- lintcode/DynamicProgramming/BackPackII.cpp
+++ lintcode/DynamicProgramming/BackPackII.cpp
@@ -18,3 +18,27 @@ public:
         return dp[m];
     }
 };
+
+
+class Solution {
+public:
+    /**
+     * @param m: An integer m denotes the size of a backpack
+     * @param A & V: Given n items with size A[i] and value V[i]
+     * @return: The maximum value
+     */
+    int backPackII(int m, vector<int> A, vector<int> V) {
+        // write your code here
+        vector<vector<int> > dp(A.size()+1, vector<int>(m+1, 0));
+
+        for (int i = 0; i < A.size(); i++) {
+            for (int j = 0; j < std::min(A[i], m+1); j++) {
+                dp[i+1][j] = dp[i][j];
+            }
+            for (int j = A[i]; j <= m; j++) {
+                dp[i+1][j] = std::max(dp[i][j], dp[i][j-A[i]] + V[i]);
+            }
+        }
+        return dp[A.size()][m];
+    }
+};
