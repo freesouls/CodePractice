940325d31d4532b4bf03db7d08e005719920544a
freesouls <declanxu@126.com>
2016-02-24 22:09:05 +0800

    update minimumAdjustmentcost

--- lintcode/DynamicProgramming/MinimumAdjustmentCost.cpp
+++ lintcode/DynamicProgramming/MinimumAdjustmentCost.cpp
@@ -6,6 +6,39 @@ public:
      */
     int MinAdjustmentCost(vector<int> A, int target) {
         // write your code here
+        vector<int> dp(101, INT32_MAX);
+        for (int i = 1; i < 101; i++) {
+            dp[i] = abs(A[0]-i); // this will guarantee dp[k]+abs(j-A[i]) not overflow
+        }
+
+        for (int i = 1; i < A.size(); i++) {
+            vector<int> new_dp(101, INT32_MAX);
+            for (int j = 1; j < 101; j++) {
+                for (int k = std::max(1, j-target); k <= std::min(100, j+target); k++) {
+                    // if (dp[k] != INT32_MAX) {
+                        new_dp[j] = std::min(new_dp[j], dp[k]+std::abs(j-A[i]));
+                    // }
+                }
+            }
+            dp = new_dp;
+        }
+
+        int res = INT32_MAX;
+        for (int i = 1; i < 101; i++) {
+            res = std::min(res, dp[i]);
+        }
+        return res;
+    }
+};
+
+class Solution {
+public:
+    /**
+     * @param A: An integer array.
+     * @param target: An integer.
+     */
+    int MinAdjustmentCost(vector<int> A, int target) {
+        // write your code here
         // dp[i][j] means when at length (i+1), minimum cost when changing A[i] to j
         vector<vector<int> > dp(A.size(), vector<int>(101, 0));
         for (int i = 1; i < 101; i++) {
@@ -21,7 +54,7 @@ public:
                 int lower = std::max(1, j - target);
 
                 for (int k = lower; k <= upper; k++) {
-                    dp[i][j] = std::min(dp[i][j], dp[i-1][k] + diff);
+                    dp[i][j] = std::min(dp[i][j], dp[i-1][k] + diff); // it is because dp[0][k] that, it won't be overflow
                 }
 
             }
