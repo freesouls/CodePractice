172996d439cf309936529a2ae83478c17a1861ca
freesouls <declanxu@126.com>
2016-02-25 22:40:22 +0800

    lintcode longestCommonSubstring

---
+++ lintcode/DynamicProgramming/longestCommonSubstring.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    /**
+     * @param A, B: Two string.
+     * @return: the length of the longest common substring.
+     */
+    int longestCommonSubstring(string &A, string &B) {
+        // write your code here
+        int m = A.length();
+        int n = B.length();
+        if (m == 0 || n == 0) {
+            return 0;
+        }
+        vector<vector<int> > dp(m+1, vector<int>(n+1, 0));
+        int res = 0;
+        // for (int i = 0; i <= m; i++) {
+        //     for (int j = 0; j <= n; j++) {
+        //         if (i == 0 || j == 0) {
+        //             dp[i][j] = 0;
+        //         }
+        //         else {
+        //             if (A[i-1] == B[j-1]) {
+        //                 dp[i][j] = dp[i-1][j-1] + 1;
+        //                 res = std::max(res, dp[i][j]);
+        //             }
+        //             else{
+        //                 dp[i][j] = 0;
+        //             }
+        //         }
+        //     }
+        // }
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (A[i] == B[j]) {
+                    dp[i+1][j+1] = dp[i][j]+1;
+                    res = std::max(res, dp[i+1][j+1]);
+                }
+            }
+        }
+        return res;
+    }
+};
