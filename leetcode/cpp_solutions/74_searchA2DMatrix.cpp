5d698c7ae5b88581c693fbebe8aa6c0b8dc7cb02
freesouls <declanxu@126.com>
2016-03-01 20:58:40 +0800

    update leetcode 74

--- leetcode/cpp_solutions/74_searchA2DMatrix.cpp
+++ leetcode/cpp_solutions/74_searchA2DMatrix.cpp
@@ -1,6 +1,34 @@
 // TODO: Binary Search
 class Solution {
 public:
+    // treat the matrix as a one-dim vector with elements ascending
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        if (matrix.size() == 0 || matrix[0].size() == 0){
+            return false;
+        }
+
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int left = 0;
+        int right = m*n-1;
+        while(left <= right) {
+            int middle = (left+right)/2;
+            int i = middle/n;
+            int j = middle%n;
+            if (matrix[i][j] < target) {
+                left = middle+1;
+            }
+            else if (matrix[i][j] > target) {
+                right = middle-1;
+            }
+            else{
+                return true;
+            }
+        }
+
+        return false;
+    }
+
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int m = matrix.size();
         int n = matrix[0].size();
