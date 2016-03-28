33e4afda55047a1d1b82174debf356f86143c6e9
freesouls <declanxu@126.com>
2016-03-28 18:58:31 +0800

    lintcode 3.28

--- lintcode/Math/PrintNumbersByRecursion.cpp
+++ lintcode/Math/PrintNumbersByRecursion.cpp
@@ -4,6 +4,45 @@ public:
      * @param n: An integer.
      * return : An array storing 1 to the largest number with n digits.
      */
+    // this version is 2x faster than below one
+    // because pop_back not erase at first element
+    vector<int> numbersByRecursion(int n) {
+        // write your code here
+        vector<int> res;
+        if (n == 0) {
+            return res;
+        }
+        dfs(res, n);
+        res.pop_back();
+        return res;
+    }
+
+    void dfs(vector<int>& res, int n) {
+        if (n == 1) {
+            for(int i = 1; i <= 10; i++) {
+                res.push_back(i);
+            }
+        }
+        else{
+            dfs(res, n-1);
+            int c_size = res.size();
+            for (int i = 1; i < 10; i++) {
+                int base = i*pow(10, n-1);
+                for (int j = 0; j < c_size; j++) {
+                    res.push_back(base + res[j]);
+                }
+            }
+        }
+    }
+};
+
+
+class Solution {
+public:
+    /**
+     * @param n: An integer.
+     * return : An array storing 1 to the largest number with n digits.
+     */
     vector<int> numbersByRecursion(int n) {
         // write your code here
         vector<int> res;
